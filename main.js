027e5b6f95853179beb004cdf9dd34102d127c8e
Ariel Cavalcante <ariel.cavalcante@edu.unifor.br>
2021-04-06 20:12:12 -0300

    Add a simple hash integrity checking

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const SHA256 = require('crypto-js/sha256');
 const express = require('express');
 
+// Servidor
 const app = express();
-
 function main(request, response, next) {
 	response.send(JSON.stringify(dinglehopperCoin, null, 4));
 }
-
 app.get('/', main);
-
 app.listen(process.env.PORT ?? 8080);
 
+// Inicia a classe bloco, com seus atributos e metodos
 class Block {
 	constructor(index, timestamp, data, previousHash = '') {
 		this.index = index;
@@ -30,6 +29,12 @@ class Block {
 	}
 }
 
+let today = new Date();
+let currentTime = `${today.getHours()}:${today.getMinutes()}:${today.getMilliseconds()} - ${today.getDate()}/${
+	today.getMonth() + 1
+}/${today.getFullYear()}`;
+
+// Inicia a classe blockchain, com seus atributos e metodos
 class Blockchain {
 	constructor() {
 		this.chain = [this.createGenesisBlock()];
@@ -48,14 +53,43 @@ class Blockchain {
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
-}
-let today = new Date();
-let currentTime = `${today.getHours()}:${today.getMinutes()}:${today.getMilliseconds()} - ${today.getDate()}/${
-	today.getMonth() + 1
-}/${today.getFullYear()}`;
 
+	isChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return `
+				~Attempt to tamper with block ${currentBlock.index}'s hash~
+				\n
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n`;
+			}
+
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return `
+				~Attempt to tamper with block ${previousBlock.index}'s hash~
+				\n
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n🚨 ATTENTION: I HAVE BEEN MURDERED 👩 🚨
+				\n`;
+			}
+		}
+
+		return '\n\n its fine \n\n';
+	}
+}
+
+// cria a moeda bruguzumba, um tipo de blockchain
 let dinglehopperCoin = new Blockchain();
 
+// faz n-1 transações de bruguzumbas de valores aleatórios (entre 0 e 20)
 for (let i = 1; i < 5; i++) {
 	dinglehopperCoin.addBlock(
 		new Block(
@@ -68,3 +102,8 @@ for (let i = 1; i < 5; i++) {
 		)
 	);
 }
+
+// dinglehopperCoin.chain[2].data = { amount: 300 };
+// dinglehopperCoin.chain[3].calculateHash();
+
+console.log(dinglehopperCoin.isChainValid());
